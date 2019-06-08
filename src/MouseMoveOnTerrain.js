748109e1d12d7e617426e8e44bc60ddf0af1a5ec
lovepsone <lovepsone@gmail.com>
2019-06-08 15:19:22 +0300

    [017] Update class MouseMoveOnTerrain
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/MouseMoveOnTerrain.js
+++ src/MouseMoveOnTerrain.js
@@ -4,8 +4,54 @@
 
 class MouseMoveOnTerrain {
 
-	constructor() {
+	constructor(_camera, _terrain, _viewport) {
 
+		const context = this;
+		this.raycaster = new THREE.Raycaster();
+		this.mouseVector = new THREE.Vector3();
+		this.camera = _camera;
+		this.geometry = _terrain;
+		this.element = document.getElementById(_viewport);
+		this.MouseDown = false;
+
+		this.element.addEventListener("mousedown", this.onDocumentMouseDown.bind(this), false);
+		this.element.addEventListener("mouseup", this.onDocumentMouseUp.bind(this), false);
+		this.element.addEventListener("mousemove", this.onDocumentMouseMove.bind(this), false);
+	}
+	
+	onDocumentMouseDown(event) {
+
+		event.preventDefault();
+		this.MouseDown = true;
+	}
+	
+	onDocumentMouseUp(event) {
+
+		event.preventDefault();
+		this.MouseDown = false;
+	}
+
+	onDocumentMouseMove(event) {
+
+		event.preventDefault();
+		if (this.MouseDown) {
+
+		}
+		
+	}
+
+	AddEvents() {
+
+		this.element.addEventListener("mousedown", this.onDocumentMouseDown.bind(this), false);
+		this.element.addEventListener("mouseup", this.onDocumentMouseUp.bind(this), false);
+		this.element.addEventListener("mousemove", this.onDocumentMouseMove.bind(this), false);
+	}
+	
+	DisposeEvents() {
+
+		this.element.removeEventListener("mousedown", this.onDocumentMouseDown, false);
+		this.element.removeEventListener("mouseup", this.onDocumentMouseUp, false);
+		this.element.removeEventListener("mousemove", this.onDocumentMouseMove.bind(this), false);
 	}
 }
 
