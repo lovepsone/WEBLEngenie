f34678326255ba44aee195491be6218014d6ffc9
lovepsone <lovepsone@gmail.com>
2019-07-26 15:44:59 +0300

    [075] Add vertexColors (bufferGeometry)
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/Terrain.js
+++ src/Terrain.js
@@ -7,6 +7,8 @@ let _spacingX = 2, _heightOffset = 2.5, _spacingZ = 2;
 let _context;
 let _worker = null;
 
+let max = 0.0, min = 0.0, center = 0.0;
+
 import {PressureTerrain} from './PressureTerrain.js';
 
 class Terrain {
@@ -36,9 +38,30 @@ class Terrain {
 		geometry.center();
 		geometry.computeFaceNormals();
 
-		_mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x0000ff, /*wireframe: true,*/ side: THREE.DoubleSide/*, morphTargets: true*/}));
+		let colors = [];
+
+		for (let i = 0, n = geometry.attributes.position.count; i < n; ++ i) {
+
+			colors[i * 3] = 0;
+			colors[i * 3 + 1] = 0;
+			colors[i * 3 + 2] = 1;
+
+			colors[i * 3 + 3] = 0;
+			colors[i * 3 + 4] = 0;
+			colors[i * 3 + 5] = 1;
+
+			colors[i * 3 + 6] = 0;
+			colors[i * 3 + 7] = 0;
+			colors[i * 3 + 8] = 1;
+		}
+
+			
+		geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+		geometry.attributes.color.needsUpdate = true;
+
+		_mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({side: THREE.DoubleSide, vertexColors: THREE.VertexColors}));
 		_mesh.name = 'Terrain';
-		_mesh.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.3, wireframe: true, transparent: true })));
+		//_mesh.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.3, wireframe: true, transparent: true })));
 
 		_scope.scene.add(_mesh);
 
@@ -55,6 +78,7 @@ class Terrain {
 			_pressure.DisposeEvents();
 			_pressure.WorkerStop();
 			_pressure = null;
+			//_worker.postMessage({'cmd':'stop'});
 
 		}
 		_depth = depth;
@@ -72,23 +96,121 @@ class Terrain {
 
 	WorkerOnMessage(e) {
 
-		let buffVertices = e.data[0], buffNormals = e.data[1];
-		let geometry = new THREE.BufferGeometry();
-		let positions = new Float32Array(buffVertices.length * 3);
-		let normals = new Float32Array(buffNormals.length * 3);
+		switch(e.data.cmd) {
 
-		geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3).copyVector3sArray(buffVertices));
-		geometry.addAttribute( 'normal', new THREE.BufferAttribute(normals, 3).copyVector3sArray(buffNormals));
-		geometry.computeBoundingBox();
-		geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-geometry.boundingBox.max.x /2, 0, -geometry.boundingBox.max.z/2));
+			case 'onLoadData': {
 
-		_mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0x0000ff, side: THREE.DoubleSide, morphTargets: true}));
-		_mesh.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.3, wireframe: true, transparent: true})));
+				let buffVertices = e.data.points, buffNormals = e.data.normals, buffColors = e.data.colors;
 
-		_scope.scene.add(_mesh);
+				max =  e.data.max;
+				min = e.data.min;
+				center = max /2;	// min = 0 в данном случаи
 
-		//_pressure = new PressureTerrain(_scope.camera, _mesh, 'Window');
-		//_pressure.AddEvents();
+				let geometry = new THREE.BufferGeometry();
+				let positions = new Float32Array(buffVertices.length * 3);
+				let normals = new Float32Array(buffNormals.length * 3);
+
+				geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3).copyVector3sArray(buffVertices));
+				geometry.addAttribute( 'normal', new THREE.BufferAttribute(normals, 3).copyVector3sArray(buffNormals));
+				geometry.addAttribute('color', new THREE.Float32BufferAttribute(buffColors, 3));
+
+				for (let i = 0; i < geometry.attributes.position.count; i++) {
+
+					if (geometry.attributes.position.array[i * 3 + 1] < max /5) {
+						//ForestGreen = 34, 139, 34
+						let fg = new THREE.Color(0xd2691e);
+						geometry.attributes.color.array[i * 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 1] = fg.g;
+						geometry.attributes.color.array[i * 3 + 2] = fg.b;
+
+						geometry.attributes.color.array[i * 3 + 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 4] = fg.g;
+						geometry.attributes.color.array[i * 3 + 5] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 6] = fg.r;
+						geometry.attributes.color.array[i * 3 + 7] = fg.g;
+						geometry.attributes.color.array[i * 3 + 8] = fg.b
+					}
+					else if (geometry.attributes.position.array[i * 3 + 1] < (max /5)*2) {
+						//Green = 0, 128, 0
+						let fg = new THREE.Color(0x8b4513);
+						geometry.attributes.color.array[i * 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 1] = fg.g;
+						geometry.attributes.color.array[i * 3 + 2] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 4] = fg.g;
+						geometry.attributes.color.array[i * 3 + 5] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 6] = fg.r;
+						geometry.attributes.color.array[i * 3 + 7] = fg.g;
+						geometry.attributes.color.array[i * 3 + 8] = fg.b	
+					}
+					else if (geometry.attributes.position.array[i * 3 + 1] < (max /5)*3) {
+						//MediumSeaGreen = 0, 128, 0
+						let fg = new THREE.Color(0x006400);
+						geometry.attributes.color.array[i * 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 1] = fg.g;
+						geometry.attributes.color.array[i * 3 + 2] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 4] = fg.g;
+						geometry.attributes.color.array[i * 3 + 5] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 6] = fg.r;
+						geometry.attributes.color.array[i * 3 + 7] = fg.g;
+						geometry.attributes.color.array[i * 3 + 8] = fg.b	
+					}
+					else if (geometry.attributes.position.array[i * 3 + 1] < (max /5)*4) {
+						let fg = new THREE.Color(0x008000);
+						geometry.attributes.color.array[i * 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 1] = fg.g;
+						geometry.attributes.color.array[i * 3 + 2] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 4] = fg.g;
+						geometry.attributes.color.array[i * 3 + 5] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 6] = fg.r;
+						geometry.attributes.color.array[i * 3 + 7] = fg.g;
+						geometry.attributes.color.array[i * 3 + 8] = fg.b			
+					}
+					else {
+						let fg = new THREE.Color(0x228b22);
+						geometry.attributes.color.array[i * 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 1] = fg.g;
+						geometry.attributes.color.array[i * 3 + 2] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 3] = fg.r;
+						geometry.attributes.color.array[i * 3 + 4] = fg.g;
+						geometry.attributes.color.array[i * 3 + 5] = fg.b
+
+						geometry.attributes.color.array[i * 3 + 6] = fg.r;
+						geometry.attributes.color.array[i * 3 + 7] = fg.g;
+						geometry.attributes.color.array[i * 3 + 8] = fg.b			
+					}
+					geometry.attributes.color.needsUpdate = true;
+				}
+
+				geometry.computeBoundingBox();
+				geometry.applyMatrix(new THREE.Matrix4().makeTranslation(-geometry.boundingBox.max.x /2, 0, -geometry.boundingBox.max.z/2));
+
+				_mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({side: THREE.DoubleSide, vertexColors: THREE.VertexColors}));
+				//_mesh.add(new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.3, wireframe: true, transparent: true})));
+				_mesh.name = 'Terrain';
+				_scope.scene.add(_mesh);
+
+				_pressure = new PressureTerrain(_scope.camera, _mesh, 'Window');
+				_pressure.AddEvents();
+
+				_worker.postMessage({'cmd': 'colors', 'points': geometry.attributes.position});
+			}
+
+			case 'onLoadColor': {
+				break;
+			}
+		}
+				
 	}
 
 	setPressureRadius(r) {
--- src/WorkerHeightMap.js
+++ src/WorkerHeightMap.js
@@ -4,10 +4,31 @@
 
 importScripts('./../libs/three.min.js');
 
+function convertFaseToPointsAndNormals(_face, _vertices, _points, _normals, _colors) {
+
+    _points.push(_vertices[_face.a], _vertices[_face.b], _vertices[_face.c]);
+    let vertexNormals = _face.vertexNormals;
+
+    if (vertexNormals.length === 3) {
+
+        _normals.push(vertexNormals[0], vertexNormals[1], vertexNormals[2]);
+    } else {
+
+        let normal = _face.normal;
+        _normals.push(normal, normal, normal);
+    }
+
+    _colors.push(0, 0, 1, 0, 0, 1, 0, 0, 0.5);
+    _colors.push(0, 0, 1, 0, 0, 0.5, 0, 0, 1);
+    _colors.push(0, 0, 0.5, 0, 0, 1, 0, 0, 1);
+}
+
 function WorkerHeightMap(pixel, size, spacing, heightOffset) {
 
-    let faces = [], vertices = [];
-    let buffVertices = [], buffNormals = [];
+    let vertices = [];
+    let buffVertices = [], buffNormals = [], buffColors = [];
+    var maxH = 0.0, minH = 0.0;
+
     let _width = _depth = size;
     let _spacingX = spacing[0], _spacingZ = spacing[1];
 
@@ -18,37 +39,36 @@ function WorkerHeightMap(pixel, size, spacing, heightOffset) {
 
             let vertex = new THREE.Vector3(x * _spacingX, pixel.data[z * 4 + (_depth * x * 4)] / heightOffset, z * _spacingZ);
             vertices.push(vertex);
+
+            if (maxH < vertex.y) maxH = vertex.y;
+            if (minH > vertex.y) minH = vertex.y;
         }
     }
 
-    //transform the pixels to faces
+    //transform the pixels to points and normals
     for (let z = 0; z < _depth - 1; z++) {
 
         for (let x = 0; x < _width - 1; x++) {
 
-            faces.push(new THREE.Face3((x + z * _width), ((x + 1) + (z * _width)), ((x + 1) + ((z + 1) * _width))));
-            faces.push(new THREE.Face3(((x + 1) + ((z + 1) * _width)), (x + ((z + 1) * _width)), (x + z * _width)));
+           convertFaseToPointsAndNormals(
+               new THREE.Face3((x + z * _width), ((x + 1) + (z * _width)), ((x + 1) + ((z + 1) * _width))),
+               vertices,
+               buffVertices,
+               buffNormals,
+               buffColors
+            );
+
+           convertFaseToPointsAndNormals(
+               new THREE.Face3(((x + 1) + ((z + 1) * _width)), (x + ((z + 1) * _width)), (x + z * _width)),
+               vertices,
+               buffVertices,
+               buffNormals,
+               buffColors
+               );
         }
     }
 
-		// преобразование в буфферную сетку 
-	for(let i = 0; i < faces.length; i ++) {
-
-		let face = faces[i];
-		buffVertices.push(vertices[face.a], vertices[face.b], vertices[face.c]);
-		let vertexNormals = face.vertexNormals;
-
-		if (vertexNormals.length === 3) {
-
-			buffNormals.push(vertexNormals[0], vertexNormals[1], vertexNormals[2]);
-		} else {
-
-			let normal = face.normal;
-			buffNormals.push(normal, normal, normal);
-		}
-    }
-
-    return [buffVertices, buffNormals];
+    return [buffVertices, buffNormals, buffColors, maxH, minH];
 }
 
 self.onmessage = function(e) {
@@ -64,7 +84,9 @@ self.onmessage = function(e) {
             break;
         case 'pixels':
             let buff = WorkerHeightMap(data.data, data.size, data.spacingXZ, data.heightOffset);
-            self.postMessage(buff);
+            self.postMessage({'cmd': 'onLoadData', 'points': buff[0], 'normals': buff[1], 'colors': buff[2], 'max': buff[3], 'min': buff[4]});
+            break;
+        case 'colors':
             break;
 
     }
