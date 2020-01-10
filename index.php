d55f89092304c8f02ca7f1b231622d253121caa5
lovepsone <lovepsone@gmail.com>
2020-01-10 19:26:57 +0300

    [128] Update project part#2
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
 	echo '<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">';
 	echo '<title></title>';
-	echo '<script type="text/javascript" src="libs/three.min.js"></script>';
 	echo '<script type="module" src="src/modules.js"></script>';
 	echo '<link rel="stylesheet" href="style.css">';
 	echo '</head><body>';
--- src/Main.js
+++ src/Main.js
@@ -4,9 +4,16 @@
 
 import {Terrain} from './Terrain.js';
 import {CameraControls} from './CameraControls.js';
+import * as THREE from './../libs/three/Three.js';
 
 let _renderer, _camera, _scene; 
 let _controls = null, _terrain = null;
+
+function lanhrag() {
+	let _masx = [0, 4, 10 , 5, 0, -2, -7];
+	let _masz = [0, 4, 8 , 10, 12, 14, 16];
+
+}
 
 class MainEngenie {
 
@@ -29,6 +36,35 @@ class MainEngenie {
 		_controls = new CameraControls(_camera, 'Window');
 		_terrain = new Terrain({scene: _scene, camera: _camera});
 
+
+	/*	var closedSpline = new THREE.CatmullRomCurve3( [
+			new THREE.Vector3( 0, 0, 0 ),
+			new THREE.Vector3( 4, 0, 4),
+			new THREE.Vector3( 10, 0, 8 ),
+			new THREE.Vector3( 5, 0, 10),
+			new THREE.Vector3( 0, 0, 12),
+			new THREE.Vector3( -2, 0, 14),
+			new THREE.Vector3( -7, 0, 16),
+			//new THREE.Vector3( 50, 0, 50)
+		] );
+		//closedSpline.curveType = 'catmullrom';
+		closedSpline.closed = false;
+		var extrudeSettings = {
+			steps: 100,
+			bevelEnabled: true,
+			extrudePath: closedSpline
+		};
+
+		var shape = new THREE.Shape();
+		shape.moveTo(0, 0);
+		shape.lineTo(0, 5);
+
+
+		var geometry = new THREE.ExtrudeBufferGeometry( shape, extrudeSettings );
+		//var geometry = new THREE.ShapeGeometry(shape);
+		var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+		var mesh = new THREE.Mesh( geometry, material );
+		_scene.add( mesh );*/
 	}
 
 	Render() {
