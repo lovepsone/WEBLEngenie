56b24b9b9a91e62bb31b5f62729334ba8c8a418d
lovepsone <lovepsone@gmail.com>
2020-08-06 12:34:11 +0300

    [200] Add constants file.
    Signed-off-by: lovepsone <lovepsone@gmail.com>

---
+++ src/CONST.js
@@ -0,0 +1,23 @@
+/*
+* Author lovepsone
+*/
+
+export const BASENAMETEXTURES =  {
+
+    OCEAN:                      './texture/ocean_dirt_512.jpg', //0x44447a,
+    BEACH:                      './texture/beach_512.jpg', //0xa09077,
+    SCORCHED:                   './texture/scorched_512.jpg',//0x555555,
+    BARE:                       './texture/bare_512.jpg',//0x888888,
+    TUNDRA:                     './texture/tundra_512.jpg',//0xbbbbaa,
+    SNOW:                       './texture/snow_512.jpg',//0xdddde4,
+    TEMPERATE_DESERT:           './texture/temperate_desert_512.jpg',//0xc9d29b,
+    TAIGA:                      './texture/taiga_512.jpg',//0x99aa77,
+	GRASSLAND :                 './texture/grass_512.jpg',//0x88aa55,
+	TEMPERATE_DECIDUOUS_FOREST: './texture/temperate_deciduous_forest_512.jpg',//0x679459,
+	TEMPERATE_RAIN_FOREST:      './texture/temperate_rain_forest_512.jpg',//0x448855,
+	SUBTROPICAL_DESERT:         './texture/subtropical_desert_512.jpg',//0xd2b98b,
+	TROPICAL_SEASONAL_FOREST:   './texture/tropical_seasonal_forest_512.jpg',//0x559944,
+	TROPICAL_RAIN_FOREST:       './texture/tropical_rain_forest_512.jpg' //0x337755
+};
+
+export const BASEDATATEXTURES = Object.entries(BASENAMETEXTURES);
--- src/TextureAtlas.js
+++ src/TextureAtlas.js
@@ -2,38 +2,20 @@
 * author lovepsone
 */
 
-const _nameTextures =  {
-
-    OCEAN:                      './texture/ocean_dirt_512.jpg', //0x44447a,
-    BEACH:                      './texture/beach_512.jpg', //0xa09077,
-    SCORCHED:                   './texture/scorched_512.jpg',//0x555555,
-    BARE:                       './texture/bare_512.jpg',//0x888888,
-    TUNDRA:                     './texture/tundra_512.jpg',//0xbbbbaa,
-    SNOW:                       './texture/snow_512.jpg',//0xdddde4,
-    TEMPERATE_DESERT:           './texture/temperate_desert_512.jpg',//0xc9d29b,
-    TAIGA:                      './texture/taiga_512.jpg',//0x99aa77,
-	GRASSLAND :                 './texture/grass_512.jpg',//0x88aa55,
-	TEMPERATE_DECIDUOUS_FOREST: './texture/temperate_deciduous_forest_512.jpg',//0x679459,
-	TEMPERATE_RAIN_FOREST:      './texture/temperate_rain_forest_512.jpg',//0x448855,
-	SUBTROPICAL_DESERT:         './texture/subtropical_desert_512.jpg',//0xd2b98b,
-	TROPICAL_SEASONAL_FOREST:   './texture/tropical_seasonal_forest_512.jpg',//0x559944,
-	TROPICAL_RAIN_FOREST:       './texture/tropical_rain_forest_512.jpg' //0x337755
-};
-
-const _datatextures = Object.entries(_nameTextures);
 let _Texture2DArray = null, _textures = [];
 let _mesh = null;
 
 import * as THREE from './../libs/three/Three.js';
+import {BASEDATATEXTURES} from './CONST.js';
 
 class TextureAtlas {
 
     constructor() {
 
-        for (let i = 0; i < _datatextures.length; i++) {
+        for (let i = 0; i < BASEDATATEXTURES.length; i++) {
 
-            _textures[i] = new THREE.TextureLoader().load(_datatextures[i][1]);
-            _textures[i].name = _datatextures[i][1]; // need fixed
+            _textures[i] = new THREE.TextureLoader().load(BASEDATATEXTURES[i][1]);
+            _textures[i].name = BASEDATATEXTURES[i][1]; // need fixed
             _textures[i].wrapS =_textures[i].wrapT = THREE.RepeatWrapping;
         }
     }
