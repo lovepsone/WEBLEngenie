0b5568aa2dbd0aa29f30cffe0fd0f52bc0a15276
lovepsone <lovepsone@gmail.com>
2020-08-10 17:21:17 +0300

    [215] Clear code

--- index.php
+++ index.php
@@ -10,32 +10,5 @@
 	echo '<title></title>';
 	echo '<script type="module" src="src/modules.js"></script>';
 	echo '<link rel="stylesheet" href="style.css">';
-	echo '</head><body>';
-	
-	echo '<script type="x-shader/x-vertex" id="vertexshader">';
-	?>
-			attribute float size;
-			attribute vec3 customColor;
-			varying vec3 vColor;
-			void main() {
-				vColor = customColor;
-				vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-				gl_PointSize = size * ( 300.0 / -mvPosition.z );
-				gl_Position = projectionMatrix * mvPosition;
-			}
-	<?php
-	echo '</script>';
-
-	echo '<script type="x-shader/x-fragment" id="fragmentshader">';
-	?>
-			uniform vec3 color;
-			uniform sampler2D texture;
-			varying vec3 vColor;
-			void main() {
-				gl_FragColor = vec4( color * vColor, 1.0 );
-				gl_FragColor = gl_FragColor * texture2D( texture, gl_PointCoord );
-				if ( gl_FragColor.a < ALPHATEST ) discard;
-			}
-	<?php
-	echo '</script></body></html>';
+	echo '</head><body></body></html>';
 ?>
