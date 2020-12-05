54783ee2d8b54d1950480b870073ca3c350eace9
lovepsone <lovepsone@gmail.com>
2020-12-05 22:43:11 +0300

    [254] Update Atlas Texture
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/TextureAtlas.js
+++ src/TextureAtlas.js
@@ -61,121 +61,125 @@ class TextureAtlas {
             console.warn('TextureAtlas.js: Create geometry before overlaying pen.');
             return;
 		}
-        let vShader = `
-            out vec2 vUv;
-
-            void main() {
-                vUv = uv;
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-            }
-        `;
-
-        let fShader = `
-            precision highp sampler2DArray;
-            precision highp float;
-            precision highp int;
-
-            uniform sampler2D OCEAN;
-            uniform sampler2D BEACH;
-            uniform sampler2D SCORCHED;
-            uniform sampler2D BARE;
-            uniform sampler2D TUNDRA;
-            uniform sampler2D SNOW;
-            uniform sampler2D TEMPERATE_DESERT;
-            uniform sampler2D TAIGA;
-            uniform sampler2D GRASSLAND;
-            uniform sampler2D TEMPERATE_DECIDUOUS_FOREST;
-            uniform sampler2D TEMPERATE_RAIN_FOREST;
-            uniform sampler2D SUBTROPICAL_DESERT;
-            uniform sampler2D TROPICAL_SEASONAL_FOREST;
-            uniform sampler2D TROPICAL_RAIN_FOREST;
-
-            in vec2 vUv;
-            uniform sampler2DArray diffuse;
-
-            float getCoord(int layer, int count) {
-
-                return max(0.0, min(float(count) - 1.0, floor(float(layer) + 0.5)));
-            }
-
-            void main() {
-
-                vec4 diff_1 = texture(diffuse, vec3(vUv, 0.0));//getCoord(0, 5)
-                vec4 diff_2 = texture(diffuse, vec3(vUv, 1.0));
-                vec4 diff_3 = texture(diffuse, vec3(vUv, 2.0));
-                vec4 diff_4 = texture(diffuse, vec3(vUv, 3.0));
-                vec4 diff_5 = texture(diffuse, vec3(vUv, 4.0));
-
-                vec4 _okean = texture2D(OCEAN, vUv*10.0);
-                vec4 _beach = texture2D(BEACH, vUv* 10.0);
-                vec4 _scorched = texture2D(SCORCHED, vUv* 10.0);
-
-                vec4 _bare = texture2D(BARE, vUv* 10.0);
-                vec4 _tundra = texture2D(TUNDRA, vUv* 10.0);
-                vec4 _snow = texture2D(SNOW, vUv* 10.0);
-
-                vec4 _temperate_desert = texture2D(TEMPERATE_DESERT, vUv* 10.0);
-                vec4 _taiga = texture2D(TAIGA, vUv* 10.0);
-                vec4 _grassland = texture2D(GRASSLAND, vUv* 10.0);
-
-                vec4 _temperate_decidious_forest = texture2D(TEMPERATE_DECIDUOUS_FOREST, vUv* 10.0);
-                vec4 _temperate_rain_forest = texture2D(TEMPERATE_RAIN_FOREST, vUv* 10.0);
-                vec4 _subtropical_desert = texture2D(SUBTROPICAL_DESERT, vUv* 10.0);
-
-                vec4 _tropical_seasonal_forest = texture2D(TROPICAL_SEASONAL_FOREST, vUv* 10.0);
-                vec4 _tropical_rain_forest = texture2D(TROPICAL_RAIN_FOREST, vUv* 10.0);
-
-                vec4 mix1 = mix(_okean, vec4(0.0, 0.0, 0.0, 1.0), diff_1.r);
-                vec4 mix2 = mix(_beach, mix1, diff_1.g);
-                vec4 mix3 = mix(_scorched, mix2, diff_1.b);
-                vec4 mix4 = mix(_bare, mix3, diff_2.r);
-                vec4 mix5 = mix(_tundra, mix4, diff_2.g);
-                vec4 mix6 = mix(_snow, mix5, diff_2.b);
-                vec4 mix7 = mix(_temperate_desert, mix6, diff_3.r);
-                vec4 mix8 = mix(_taiga, mix7, diff_3.g);
-                vec4 mix9 = mix(_grassland, mix8, diff_3.b);
-                vec4 mix10 = mix(_temperate_decidious_forest, mix9, diff_4.r);
-                vec4 mix11 = mix(_temperate_rain_forest, mix10, diff_4.g);
-                vec4 mix12 = mix(_subtropical_desert, mix11, diff_4.b);
-                vec4 mix13 = mix(_tropical_seasonal_forest, mix12, diff_5.r);
-                vec4 mix14 = mix(_tropical_rain_forest, mix13, diff_5.g);
-                gl_FragColor = mix14;
-            }
-            `;
-
-        let customUniforms = {
-            diffuse:                    {value: _Texture2DArray},
-            OCEAN:	                    {type: "t", value: _textures[0]},
-            BEACH:	                    {type: "t", value: _textures[1]},
-            SCORCHED:	                {type: "t", value: _textures[2]},
-            BARE:	                    {type: "t", value: _textures[3]},
-            TUNDRA:	                    {type: "t", value: _textures[4]},
-            SNOW:	                    {type: "t", value: _textures[5]},
-            TEMPERATE_DESERT:	        {type: "t", value: _textures[6]},
-            TAIGA:	                    {type: "t", value: _textures[7]},
-            GRASSLAND:	                {type: "t", value: _textures[8]},
-            TEMPERATE_DECIDUOUS_FOREST:	{type: "t", value: _textures[9]},
-            TEMPERATE_RAIN_FOREST:	    {type: "t", value: _textures[10]},
-            SUBTROPICAL_DESERT:	        {type: "t", value: _textures[11]},
-            TROPICAL_SEASONAL_FOREST:	{type: "t", value: _textures[12]},
-            TROPICAL_RAIN_FOREST:	    {type: "t", value: _textures[13]},
-        };
 
         if (_ChangeBiomes) {
 
-            _material = new THREE.ShaderMaterial({
-                uniforms: customUniforms,
-                vertexShader: vShader,
-                fragmentShader: fShader,
-                wireframe: wireframe
-            });
-    
+            _material = new THREE.MeshLambertMaterial();
+
+            _material.onBeforeCompile = function(shader) {
+
+                shader.uniforms.diffuses = {value: _Texture2DArray};
+                shader.uniforms.OCEAN = {type: "t", value: _textures[0]};
+                shader.uniforms.BEACH = {type: "t", value: _textures[1]};
+                shader.uniforms.SCORCHED = {type: "t", value: _textures[2]};
+                shader.uniforms.BARE = {type: "t", value: _textures[3]};
+                shader.uniforms.TUNDRA = {type: "t", value: _textures[4]};
+                shader.uniforms.SNOW = {type: "t", value: _textures[5]};
+                shader.uniforms.TEMPERATE_DESERT = {type: "t", value: _textures[6]};
+                shader.uniforms.TAIGA = {type: "t", value: _textures[7]};
+                shader.uniforms.GRASSLAND = {type: "t", value: _textures[8]};
+                shader.uniforms.TEMPERATE_DECIDUOUS_FOREST = {type: "t", value: _textures[9]};
+                shader.uniforms.TEMPERATE_RAIN_FOREST = {type: "t", value: _textures[10]};
+                shader.uniforms.SUBTROPICAL_DESERT = {type: "t", value: _textures[11]};
+                shader.uniforms.TROPICAL_SEASONAL_FOREST = {type: "t", value: _textures[12]};
+                shader.uniforms.TROPICAL_RAIN_FOREST = {type: "t", value: _textures[13]};
+
+                shader.vertexShader = 'out vec2 vUv;\n'  + shader.vertexShader;
+                shader.vertexShader = shader.vertexShader.replace(
+                    '#include <begin_vertex>',
+                    [
+                        '#include <begin_vertex>',
+                        'vUv = uv;',
+                        'gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
+                    ].join('\n')
+                );
+
+                shader.fragmentShader = [
+                    'precision highp sampler2DArray;',
+                    //'precision highp float;',
+                    //'precision highp int;',
+                    'in vec2 vUv;',
+                    'uniform sampler2D OCEAN;',
+                    'uniform sampler2D BEACH;',
+                    'uniform sampler2D SCORCHED;',
+                    'uniform sampler2D BARE;',
+                    'uniform sampler2D TUNDRA;',
+                    'uniform sampler2D SNOW;',
+                    'uniform sampler2D TEMPERATE_DESERT;',
+                    'uniform sampler2D TAIGA;',
+                    'uniform sampler2D GRASSLAND;',
+                    'uniform sampler2D TEMPERATE_DECIDUOUS_FOREST;',
+                    'uniform sampler2D TEMPERATE_RAIN_FOREST;',
+                    'uniform sampler2D SUBTROPICAL_DESERT;',
+                    'uniform sampler2D TROPICAL_SEASONAL_FOREST;',
+                    'uniform sampler2D TROPICAL_RAIN_FOREST;',
+                    'uniform sampler2DArray diffuses;',
+
+                    'float getCoord(int layer, int count) {',
+
+                    '    return max(0.0, min(float(count) - 1.0, floor(float(layer) + 0.5)));',
+                    '}',
+                ].join('\n') + shader.fragmentShader;
+
+                shader.fragmentShader = shader.fragmentShader.replace(
+                    '#include <dithering_fragment>',
+                    [
+                        '#include <dithering_fragment>',
+
+                        'vec4 diff_1 = texture(diffuses, vec3(vUv, 0.0));//getCoord(0, 5)',
+                        'vec4 diff_2 = texture(diffuses, vec3(vUv, 1.0));',
+                        'vec4 diff_3 = texture(diffuses, vec3(vUv, 2.0));',
+                        'vec4 diff_4 = texture(diffuses, vec3(vUv, 3.0));',
+                        'vec4 diff_5 = texture(diffuses, vec3(vUv, 4.0));',
+
+                        'vec4 _okean = texture2D(OCEAN, vUv*10.0);',
+                        'vec4 _beach = texture2D(BEACH, vUv* 10.0);',
+                        'vec4 _scorched = texture2D(SCORCHED, vUv* 10.0);',
+
+                        'vec4  _bare = texture2D(BARE, vUv* 10.0);',
+                        'vec4 _tundra = texture2D(TUNDRA, vUv* 10.0);',
+                        'vec4 _snow = texture2D(SNOW, vUv* 10.0);',
+
+                        'vec4 _temperate_desert = texture2D(TEMPERATE_DESERT, vUv* 10.0);',
+                        'vec4 _taiga = texture2D(TAIGA, vUv* 10.0);',
+                        'vec4 _grassland = texture2D(GRASSLAND, vUv* 10.0);',
+
+                        'vec4 _temperate_decidious_forest = texture2D(TEMPERATE_DECIDUOUS_FOREST, vUv* 10.0);',
+                        'vec4 _temperate_rain_forest = texture2D(TEMPERATE_RAIN_FOREST, vUv* 10.0);',
+                        'vec4 _subtropical_desert = texture2D(SUBTROPICAL_DESERT, vUv* 10.0);',
+
+                        'vec4 _tropical_seasonal_forest = texture2D(TROPICAL_SEASONAL_FOREST, vUv* 10.0);',
+                        'vec4 _tropical_rain_forest = texture2D(TROPICAL_RAIN_FOREST, vUv* 10.0);',
+
+                        'vec4 _mix = mix(_okean, vec4(0.0, 0.0, 0.0, 1.0), diff_1.r);',
+                        '_mix = mix(_beach, _mix, diff_1.g);',
+                        '_mix = mix(_scorched, _mix, diff_1.b);',
+                        '_mix = mix(_bare, _mix, diff_2.r);',
+                        '_mix = mix(_tundra, _mix, diff_2.g);',
+                        '_mix = mix(_snow, _mix, diff_2.b);',
+                        '_mix = mix(_temperate_desert, _mix, diff_3.r);',
+                        '_mix = mix(_taiga, _mix, diff_3.g);',
+                        '_mix = mix(_grassland, _mix, diff_3.b);',
+                        '_mix = mix(_temperate_decidious_forest, _mix, diff_4.r);',
+                        '_mix = mix(_temperate_rain_forest, _mix, diff_4.g);',
+                        '_mix = mix(_subtropical_desert, _mix, diff_4.b);',
+                        '_mix = mix(_tropical_seasonal_forest, _mix, diff_5.r);',
+                        '_mix = mix(_tropical_rain_forest, _mix, diff_5.g);',
+
+                        //'gl_FragColor = _mix;'
+                        'gl_FragColor *= _mix;'
+                    ].join('\n')
+                );
+                _material.userData.shader = shader;
+            }
         }
 
         if ( _material != null) {
 
             _mesh.material = _material;
             _mesh.material.needsUpdate = true;
+            _mesh.castShadow = true;
+            _mesh.receiveShadow = true;
         }
 
         _ChangeBiomes = false;
