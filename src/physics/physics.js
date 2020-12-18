94e6ad7e553bedfbd77cfa7b4d43f37da22e3925
lovepsone <lovepsone@gmail.com>
2020-12-18 19:15:16 +0300

    [272] Delete Capsule geometry
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/physics/physics.js
+++ src/physics/physics.js
@@ -7,52 +7,6 @@ import {RigidBody} from './RigidBody.js';
 import {Character} from './Character.js'
 import {STATE, FLAG, GROUP, GeometryInfo} from './flags.js';
 
-function Capsule( radius, height, radialSegs, heightSegs ) {
-
-	THREE.BufferGeometry.call( this );
-
-	this.type = 'Capsule';
-
-    radius = radius || 1;
-    height = height || 1;
-
-    var pi = Math.PI;
-
-    radialSegs = Math.floor( radialSegs ) || 12;
-    var sHeight = Math.floor( radialSegs * 0.5 );
-
-    heightSegs = Math.floor( heightSegs ) || 1;
-    var o0 = Math.PI * 2;
-    var o1 = Math.PI * 0.5;
-    var g = new THREE.Geometry();
-    var m0 = new THREE.CylinderGeometry( radius, radius, height, radialSegs, heightSegs, true );
-
-    var mr = new THREE.Matrix4();
-    var m1 = new THREE.SphereGeometry( radius, radialSegs, sHeight, 0, o0, 0, o1);
-    var m2 = new THREE.SphereGeometry( radius, radialSegs, sHeight, 0, o0, o1, o1);
-    var mtx0 = new THREE.Matrix4().makeTranslation( 0,0,0 );
-   // if(radialSegs===6) mtx0.makeRotationY( 30 * THREE.Math.DEG2RAD );
-    var mtx1 = new THREE.Matrix4().makeTranslation(0, height*0.5,0);
-    var mtx2 = new THREE.Matrix4().makeTranslation(0, -height*0.5,0);
-    mr.makeRotationZ( pi );
-    g.merge( m0, mtx0.multiply(mr) );
-    g.merge( m1, mtx1);
-    g.merge( m2, mtx2);
-
-    g.mergeVertices();
-    g.computeVertexNormals();
-
-    m0.dispose();
-    m1.dispose();
-    m2.dispose();
-
-    this.fromGeometry( g );
-
-    g.dispose();
-}
-Capsule.prototype = Object.create(THREE.BufferGeometry.prototype);
-
-
 const clock = new THREE.Clock();
 let _worker = null, _scope = null;
 
@@ -289,8 +243,9 @@ export class Physics {
         option.position = option.position || _option.controller.getPosition();
         //option.quaternion
 
-        let mesh = new THREE.Mesh(new Capsule(option.size[0], option.size[1], 6), new THREE.MeshBasicMaterial({ color: 0x993399/*, wireframe: true*/}));
+        let mesh = new THREE.Mesh(new THREE.BoxBufferGeometry(option.size[0], option.size[1], 1), new THREE.MeshBasicMaterial({ color: 0x993399/*, wireframe: true*/}));
         mesh.position.fromArray(option.position);
+        mesh.visible = false;
         _Character.add(mesh, option);
         this.send('add', option);
         return mesh;
