5aa6132148ec360717548bda2652b4f99ccd0285
lovepsone <lovepsone@gmail.com>
2021-10-09 13:43:27 +0300

    [299] Implementation of texture mapping on the road
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/Road.js
+++ src/Road.js
@@ -29,44 +29,16 @@ const WorldUVGenerator = {
 			new THREE.Vector2( b_x, b_y ),
 			new THREE.Vector2( c_x, c_y )
 		];
-
 	},
 
 	generateSideWallUV: function (geometry, vertices, indexA, indexB, indexC, indexD) {
 
-		const a_x = vertices[indexA * 3];
-		const a_y = vertices[indexA * 3 + 1];
-		const a_z = vertices[indexA * 3 + 2];
-		const b_x = vertices[indexB * 3 ];
-		const b_y = vertices[indexB * 3 + 1];
-		const b_z = vertices[indexB * 3 + 2];
-		const c_x = vertices[indexC * 3 ];
-		const c_y = vertices[indexC * 3 + 1];
-		const c_z = vertices[indexC * 3 + 2];
-		const d_x = vertices[indexD * 3 ];
-		const d_y = vertices[indexD * 3 + 1];
-		const d_z = vertices[indexD * 3 + 2];
-
-		console.log(Math.abs(a_y - b_y))
-		if (Math.abs(a_y - b_y) < 0.01) {
-
-			return [
-				new THREE.Vector2( a_x, 1 - a_z),
-				new THREE.Vector2( b_x, 1 - b_z),
-				new THREE.Vector2( c_x, 1 - c_z),
-				new THREE.Vector2( d_x, 1 - d_z)
-			];
-
-		} else {
-
-			return [
-				new THREE.Vector2(a_y, 1 - a_z),
-				new THREE.Vector2(b_y, 1 - b_z),
-				new THREE.Vector2(c_y, 1 - c_z),
-				new THREE.Vector2(d_y, 1 - d_z)
-			];
-		}
-
+		return ([
+				new THREE.Vector2(0, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 1),
+				new THREE.Vector2(0, 1)
+		]);
 	}
 };
 
@@ -186,7 +158,7 @@ class Road {
 	
 		for (let i = 0; i < _boxes.length; i++) {
 
-			points.push(new THREE.Vector3().copy(_boxes[i].position)/*_boxes[i].position*/);
+			points.push(new THREE.Vector3().copy(_boxes[i].position));
 			_scene.remove(_boxes[i]);
 
 			if (i < _lines.length)
@@ -198,6 +170,7 @@ class Road {
 			console.warn('Road.js: No direction points.');
 			return;
 		}
+
 		let shape = new THREE.Shape();
 		shape.moveTo(0, 0);
 		shape.lineTo(0, 5);
@@ -209,50 +182,10 @@ class Road {
 		let extrudeSettings = {steps: 10 * points.length, bevelEnabled: false, extrudePath: new THREE.CatmullRomCurve3(points, false), UVGenerator: WorldUVGenerator};
 		let extrudeGeometry = new THREE.ExtrudeBufferGeometry(shape, extrudeSettings);
 	
-		let texture = new THREE.TextureLoader().load(/*"texture/asphalt1_512.jpg"*/"asphalt2_512.jpg");
-		/*texture.wrapS = */texture.wrapT = THREE.RepeatWrapping;
+		let texture = new THREE.TextureLoader().load("road/road.jpg");
+		texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
 
-		let vShader = `
-			out vec2 vUv;
-			out vec3 vPosition;
-
-			void main() {
-				vUv = uv;
-				vPosition = position;
-				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-			}
-		`;
-
-		let fShader = `
-			precision highp sampler2DArray;
-			precision highp float;
-			precision highp int;
-			uniform sampler2D text;
-			in vec2 vUv;
-			in vec3 vPosition;
-
-			void main() {
-
-				vec4 _texture = texture2D(text, fract(vUv * 1.0));
-
-				gl_FragColor = _texture;
-			}
-		`;
-
-		let customUniforms = {
-
-			text: {type: "t", value: texture},
-		};
-
-		let _material = new THREE.ShaderMaterial({
-
-			uniforms: customUniforms,
-			vertexShader: vShader,
-			fragmentShader: fShader,
-			wireframe: wireframe
-		});
-
-		_roads.push(new THREE.Mesh(extrudeGeometry, /*_material*/new THREE.MeshPhongMaterial({map: texture, wireframe: wireframe})));
+		_roads.push(new THREE.Mesh(extrudeGeometry, new THREE.MeshPhongMaterial({map: texture, wireframe: wireframe})));
 		_scene.add(_roads[_roads.length - 1]);
 		_roads[_roads.length - 1].position.y += 0.05;
 
