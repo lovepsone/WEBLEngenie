61060b631db145eaa6aed9fc9a3e4829d0ab626e
lovepsone <lovepsone@gmail.com>
2021-10-13 12:13:55 +0300

    [304] Fixed errors brush biome map
    Signed-off-by: lovepsone <lovepsone@gmail.com>

--- src/GenerateBiomeMap.js
+++ src/GenerateBiomeMap.js
@@ -3,7 +3,7 @@
 */
 
 import * as THREE from '../libs/three.module.js';
-import {sphereIntersectTriangle} from './../libs/BVH/Utils/MathUtilities.js';
+import {CONTAINED, INTERSECTED, NOT_INTERSECTED} from './../libs/BVH/index.js';
 
 let _canvas = document.createElement('canvas'), _ctx = null;
 let _width = 128, _height = 128, _matrix = [];
@@ -13,7 +13,6 @@ let _ColorsCanvas = document.createElement('canvas');
 let bindMouseDown, bindMouseUp, bindMouseMove;
 let _brushMesh = null, _camera = null, _scene = null, _mesh = null;
 let _mouseVector = new THREE.Vector2();
-let _raycaster = new THREE.Raycaster();
 let _MouseDown = false;
 let _ColorPen = new THREE.Color(0x44447a);
 let _radius = 0.1;
@@ -29,8 +28,23 @@ class GenerateBiomeMap {
 		bindMouseMove = this.onDocumentMouseMove.bind(this);
         bindMouseUp = this.onDocumentMouseUp.bind(this);
 
-		_brushMesh = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 50, 50), new THREE.MeshStandardMaterial({color: 0xEC407A, roughness: 0.75, metalness: 0, transparent: true, opacity: 0.5, premultipliedAlpha: true, emissive: 0xEC407A, emissiveIntensity: 0.5}));
-        _brushMesh.name = "BrushRoad";
+        const brushSegments = [new THREE.Vector3(), new THREE.Vector3(0, 1, 0)];
+		for (let i = 0; i < 50; i ++) {
+	
+			const nexti = i + 1;
+			const x1 = Math.sin( 2 * Math.PI * i / 50);
+			const y1 = Math.cos( 2 * Math.PI * i / 50);
+			const x2 = Math.sin(2 * Math.PI * nexti / 50);
+			const y2 = Math.cos(2 * Math.PI * nexti / 50);
+	
+			brushSegments.push(new THREE.Vector3(x1, 0, y1), new THREE.Vector3(x2, 0, y2));
+		}
+
+		//_brushMesh = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 50, 50), new THREE.MeshStandardMaterial({color: 0xEC407A, roughness: 0.75, metalness: 0, transparent: true, opacity: 0.5, premultipliedAlpha: true, emissive: 0xEC407A, emissiveIntensity: 0.5}));
+		_brushMesh = new THREE.LineSegments();
+		_brushMesh.geometry.setFromPoints(brushSegments);
+		_brushMesh.material.color.set(0xEC407A);
+        _brushMesh.name = "BrushBiome";
         _brushMesh.visible = false;
 
         _scene.add(_brushMesh);
@@ -96,53 +110,79 @@ class GenerateBiomeMap {
             return;
         }
 
-		event.preventDefault();
-
 		_mouseVector.x = (event.layerX / window.innerWidth) * 2 - 1;
         _mouseVector.y = - (event.layerY / window.innerHeight) * 2 + 1;
-        _raycaster.setFromCamera(_mouseVector, _camera);
-		_raycaster.firstHitOnly = true;
 
-		let intersects = _raycaster.intersectObject(_mesh);
-		let bvh = _mesh.geometry.boundsTree;
+        const raycaster = new THREE.Raycaster();
+		raycaster.setFromCamera(_mouseVector, _camera);
+		raycaster.firstHitOnly = true;
+
+		const intersects = raycaster.intersectObject(_mesh, true)[0];
+		const bvh = _mesh.geometry.boundsTree;
 		_brushMesh.scale.setScalar(_radius);
         _brushMesh.visible = false;
 
-		if (intersects.length > 0) {
+		if (intersects) {
 
-			_brushMesh.position.copy(intersects[0].point);
+			_brushMesh.position.copy(intersects.point);
             _brushMesh.visible = true;
 
 			if (_MouseDown) {
 
-				const indices = [];
+				const indices = new Set(), tempVec = new THREE.Vector3();
 				const sphere = new THREE.Sphere(_brushMesh.position, _radius);
-                let colorAttr = _mesh.geometry.getAttribute('color');
+                const colorAttr = _mesh.geometry.getAttribute('color');
 				const indexAttr =_mesh.geometry.index;
 
-				bvh.shapecast(_mesh, box => sphere.intersectsBox(box), (tri, a, b, c) => 
-				{
-					
-					if (sphereIntersectTriangle(sphere, tri)) {
+                bvh.shapecast({
 
-						indices.push(a, b, c);
-					}
-	
-					return false;
-	
-				});
-				
-				for (let i = 0, l = indices.length; i < l; i ++ ) {
+                    intersectsBounds: (box, isLeaf, score, depth, nodeIndex) => {
 
-                    const index = indexAttr.getX(indices[i]);
-                    colorAttr.setX(index, _ColorPen.r);
-                    colorAttr.setY(index, _ColorPen.g);
-                    colorAttr.setZ(index, _ColorPen.b);
+                        const intersects = sphere.intersectsBox(box), {min, max} = box;
+        
+                        if (intersects) {
+                            for (let x = 0; x <= 1; x ++) {
+                                for (let y = 0; y <= 1; y ++) {
+                                    for (let z = 0; z <= 1; z ++ ) {
+            
+                                        tempVec.set(x === 0 ? min.x : max.x, y === 0 ? min.y : max.y, z === 0 ? min.z : max.z);
+                                        if (!sphere.containsPoint(tempVec)) return INTERSECTED;
+                                    }
+                                }
+                            }
+                            return CONTAINED;
+                        }
+                        return intersects ? INTERSECTED : NOT_INTERSECTED;
+                    },
+        
+                    intersectsTriangle: (tri, index, contained) => {
+
+                        const i3 = 3 * index, a = i3 + 0, b = i3 + 1, c = i3 + 2;
+                        const va = indexAttr.getX(a), vb = indexAttr.getX(b), vc = indexAttr.getX(c);
+        
+                        if (contained) {
+
+                            indices.add(va);
+                            indices.add(vb);
+                            indices.add(vc);
+                        } else {
+
+                            if (sphere.containsPoint(tri.a)) indices.add(va);
+                            if (sphere.containsPoint(tri.b)) indices.add(vb);
+                            if (sphere.containsPoint(tri.c)) indices.add(vc);
+                        }
+                        return false;
+                    }
+                });
+
+                indices.forEach(index => {
+
+                    colorAttr.setXYZ(index, _ColorPen.r, _ColorPen.g, _ColorPen.b);
                     const x = index % _width;
                     const y = Math.floor(index/_width);
                     _ctx.fillStyle = '#' + _ColorPen.getHexString();
-                    _ctx.fillRect(x, y, 1, 1);    
-				}
+                    _ctx.fillRect(x, y, 1, 1);
+                });
 				colorAttr.needsUpdate = true;
 			}
 		}
